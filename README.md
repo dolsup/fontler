71a1e7c32696a063a61236873a85c8d2bc9cde15
Jiwon Choi <1890mah@gmail.com>
2015-06-27 12:28:28 +0900

    init

---
+++ README.md
@@ -0,0 +1,21 @@
+# fontler
+## feature
+- create subset of font with specified string
+- woff / eot output
+- 글꼴에서 지정한 문자열만 남기기
+- woff / eot 파일 생성
+
+## install
+`npm i fontler --save`
+
+## usage
+```js
+   var fontler = require('fontler');
+   fontler('originalFont.ttf', 'subset.woff', '첩첩산 방방곡곡 굽굽이 찾아들어', 'woff', function(err) {
+	if(err) console.log(err);
+	else console.log("OK");
+   });
+```
+
+## licence
+THE BEER-WARE LICENSE (Revision 42)
---
+++ fontler.js
@@ -0,0 +1,56 @@
+require('colors');
+var fs = require('fs');
+    
+var spawn = require('child_process').spawn;    
+
+function subset(inputFile, outputFile, subString, outputFormat, callback) {
+    checkFile(inputFile, function(err) {
+        if(err) {
+            callback(err);
+        } else {
+            var ops = ['-s', subString, (outputFormat==='eot')?'-e':((outputFormat==='woff')?'-w':'')];
+            snftly(inputFile, outputFile, ops, function(code) {
+                callback(code);
+            });
+        }
+    });
+}
+
+process.on('uncaughtException', function (err) {
+    console.log("FONTLER : ".red + err);
+});
+
+function checkFile(inputFile, callback) {
+    var invalid = true;
+    try {
+        if(inputFile.split('.').pop()=='otf') throw "Sorry, OTF fonts are not supported";
+        else {
+            try{
+                if(fs.statSync(inputFile)) invalid = false;
+            } catch(e) {
+                if(e) throw "..Where's the file? (" + inputFile + ")";
+            }
+        }
+    } catch(err) {
+        invalid = "FONTLER : ".red + err;
+    } finally {
+        callback(invalid);
+    }
+}
+
+function snftly(fontfile, outfile, options, callback) {
+    var ops = ['-classpath', __dirname + '/lib/sfntly.jar', 'com.google.typography.font.tools.sfnttool.SfntTool'].concat(options).concat([fontfile, outfile]);
+    
+    var cmd = spawn('java', ops);
+    cmd.stdout.on('data', function(data){
+        //console.log(data.toString());
+    });
+    cmd.stderr.on('data', function(data){
+        console.log("FONTLER : ".red + data.toString());
+    });
+    cmd.on('close', function(code){
+        callback(code);
+    });
+}
+
+module.exports = subset
